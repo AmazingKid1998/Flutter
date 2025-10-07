dfb4d3ef87326697b5fcfbeb3dea8586912d807b
AmazingKid1998 <sheium1998@gmail.com>
2025-10-07 13:21:29 +0100

    seeing an edit

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'ethan wants to see how long it takes'),
     );
   }
 }
